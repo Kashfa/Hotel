1f1dc8da777213e6aa7d10690ab751c4cdc06d08
Kashfa <cyan82@hotmail.com>
2018-03-11 13:14:00 +0000

    added Room type

---
+++ src/main/java/Guest/Guest.java
@@ -0,0 +1,22 @@
+public class Guest {
+
+    private String name;
+    private int wallet;
+
+    public Guest(String name, int wallet) {
+        this.name = name;
+        this.wallet = wallet;
+
+    }
+
+
+    public String getName() {
+        return this.name;
+    }
+
+
+    public int getWallet() {
+        return this.wallet;
+    }
+}
+
